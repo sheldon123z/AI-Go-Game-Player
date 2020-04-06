8f167c198e3034b36c620a4f48b8aa0aaa24cfdf
sheldon123z <zhen371@usc.edu>
2020-04-06 16:45:18 -0700

    updated test.py

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ A Go game player, using min-max alpha-beta, greedy, Q learning technics to play 
 ## RUN:
 * Under the same directory run ``sh build.sh`` to play 2 games with random player
 * Modify ``build.sh`` line 95 to play more games at one time
+* Run ``test.py`` to clean up 
 
--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 import numpy as numpy
 import json
 
-def write_moves(result,path="/Users/xiaodongzheng/OneDrive - University of Southern California/USC/Classes/CSCI 561 Artificial Intelligence/HW/HW2/Go_game_player/moves.txt"):
-    with open(path,'w') as output:
+path="/Users/xiaodongzheng/OneDrive - University of Southern California/USC/Classes/CSCI 561 Artificial Intelligence/HW/HW2/Go_game_player/"
+
+def write_moves(result,fpath= path + "moves.txt"):
+    with open(fpath,'w') as output:
         output.write(result)
 
 write_moves("0")
-f = open ('/Users/xiaodongzheng/OneDrive - University of Southern California/USC/Classes/CSCI 561 Artificial Intelligence/HW/HW2/Go_game_player/history_states.txt','w').close()
-j = open ('/Users/xiaodongzheng/OneDrive - University of Southern California/USC/Classes/CSCI 561 Artificial Intelligence/HW/HW2/Go_game_player/train_set.json','w').close()
-
+f = open (path + 'history_states.txt','w').close()
+j = open (path + 'train_set.json','w').close()
+j = open (path + 'cresult.txt','w').close()
 
 
 
